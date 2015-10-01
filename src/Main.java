1270baedf9f009e23a926607fa0b25c6a1acb404
Austin Cook <aac6012@rit.edu>
2015-10-01 17:23:41 -0400

    Initial IntelliJ IDEA Project commit.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Austin on 10/1/2015.
+ * Test file.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello project!") ;
+
+    }
+
+}
