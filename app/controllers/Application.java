2af498d56c5682c22a6ea7231a45243137357d4f
David Thong Nguyen <thnguyen2808@gmail.com>
2015-10-19 16:39:36 -0400

    Unstable! Designing in progress, only uploading assets

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
     public static final String USER_ROLE = "user";
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render(""));
     }
 
 }
