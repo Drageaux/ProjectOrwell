485af30c5a65fb9dc81a9a545a8b6a668b13e905
David Thong Nguyen <thnguyen2808@gmail.com>
2015-10-19 22:49:30 -0400

    Figured out how to include, moving on to working with topbar and css

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
     public static final String USER_ROLE = "user";
     public Result index() {
-        return ok(index.render(""));
+        return ok(index.render());
     }
 
 }
