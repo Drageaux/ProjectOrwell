314f9ad8b79dd267812197bd25e4ebef79358574
Austin Cook <aac6012@rit.edu>
2015-12-05 14:58:10 -0500

    Added new model for facebook entries.

---
+++ app/models/entries/CheckinEntry.java
@@ -0,0 +1,70 @@
+package models.entries;
+
+import models.LinkedAccount;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Austin on 12/5/2015.
+ * Model for entries created from Facebook check-ins
+ */
+public class CheckinEntry extends Entry{
+
+    String location ;
+
+    String taggedFriends ;
+
+    public CheckinEntry(){
+        super() ;
+    }
+
+    public CheckinEntry(String loc, String friends){
+        super() ;
+        this.location = loc ;
+        this.taggedFriends = friends ;
+    }
+    
+    /***********************************************************
+     * Create and return a new CheckinEntry object.
+     ***********************************************************/
+    public CheckinEntry create(long linkedAccountId, Date date, String loc, String friends){
+
+        //Find the LinkedAccount object using the id
+        LinkedAccount linked = LinkedAccount.find.where().eq("providerUserId",""+linkedAccountId).findUnique() ;
+        List<LinkedAccount> accounts = new ArrayList<LinkedAccount>();
+        accounts.add(linked);
+
+        CheckinEntry entry = new CheckinEntry() ;
+
+        //General entry fields
+        entry.setStartTime(date) ;
+        entry.setEndTime(date) ; // (start time) == (end time) because event is instantaneous
+        entry.setLinkedAccounts(null) ;
+
+        //Checkin specific fields
+        entry.setLocation(loc) ;
+        entry.setTaggedFriends(friends) ;
+        return entry ;
+    }
+
+
+    /********************************************
+     * Field getters & setters
+     ********************************************/
+    public void setLocation(String loc){
+        this.location = loc ;
+    }
+    public String getLocation(){
+        return this.location ;
+    }
+
+    public void setTaggedFriends(String friends){
+        this.taggedFriends = friends ;
+    }
+    public String getTaggedFriends(){
+        return this.taggedFriends ;
+    }
+
+}
